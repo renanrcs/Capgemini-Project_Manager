bc122560564671290d8ca8aa6725c60b3ac27a5d
renanrcs <renan.cavalcante@yahoo.com>
2022-07-12 07:49:38 -0300

    metodo remover

--- controller/TaskController.java
+++ controller/TaskController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.Task;
+import util.ConnectionFactory;
 
 public class TaskController {
 	
@@ -14,7 +18,23 @@ public class TaskController {
 		
 	}
 	
-	public void removeById(int taskId) {
+	public void removeById(int taskId) throws SQLException {
+		
+		String sql = "DELETE FROM tasks WHERE id = ?";
+		
+		Connection conn = null;
+		PreparedStatement statement = null;
+		
+		try {
+			conn = ConnectionFactory.getConnection();
+			statement = conn.prepareStatement(sql);
+			statement.setInt(1, taskId);
+			statement.execute();
+		} catch (SQLException e) {
+			throw new SQLException("Erro ao deletar a tarefa");
+		}finally {
+			ConnectionFactory.closeConnection(conn);
+		}
 		
 	}
 	
