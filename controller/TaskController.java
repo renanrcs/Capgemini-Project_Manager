f6f529c307e380ed788d91ef42c88e62afcfa239
renanrcs <renan.cavalcante@yahoo.com>
2022-07-12 08:26:04 -0300

    implementando metodo save

--- controller/TaskController.java
+++ controller/TaskController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -11,6 +12,31 @@ import util.ConnectionFactory;
 public class TaskController {
 	
 	public void save(Task task) {
+		
+		String sql = "INSERT INTO tasks (idProject, name, description, "
+				+ "status, notes, deadline, createdAt, updatedAt) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		
+		try {
+			connection = ConnectionFactory.getConnection();
+			statement = connection.prepareStatement(sql);
+			
+			statement.setInt(1, task.getIdProject());
+			statement.setString(2, task.getName());
+			statement.setString(3, task.getDescription());
+			statement.setBoolean(4, task.isCompleted());
+			statement.setString(5, task.getNotes());
+			statement.setDate(6, new Date(task.getDeadline().getTime()));
+			statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+			statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+			
+			statement.execute();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 	}
 	
