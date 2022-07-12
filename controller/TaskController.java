d775cc9270cf40f1e7982adeeabf4a75bd1f3c21
renanrcs <renan.cavalcante@yahoo.com>
2022-07-12 07:32:54 -0300

    criação dos controladores

---
+++ controller/TaskController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.List;
+
+import model.Task;
+
+public class TaskController {
+	
+	public void save(Task task) {
+		
+	}
+	
+	public void update(Task task) {
+		
+	}
+	
+	public void removeById(int taskId) {
+		
+	}
+	
+	public List<Task> getAll(int idProject) {
+		return null;
+	}
+}
--- projectManager/Main.java
+++ projectManager/Main.java
@@ -1,8 +1,18 @@
 package projectManager;
 
+import java.sql.Connection;
+
+import util.ConnectionFactory;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Project On Fire");
+		
+		Connection c = (Connection) new ConnectionFactory();
+		
+		ConnectionFactory.closeConnection(c);
 		
 		
 
